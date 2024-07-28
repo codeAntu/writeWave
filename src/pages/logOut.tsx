0acaa2644c32d346d8433ec541b8ec7e55248ff0
Ananta <codeantu@gmail.com>
2024-07-28 21:54:43 +0530

    chore: Refactor LogOut component and update logout functionality

--- src/pages/logOut.tsx
+++ src/pages/logOut.tsx
@@ -1,21 +1,25 @@
 import { authService } from "@/appWrite/auth";
-
+import { useState } from "react";
+import { useStore } from "@/store/store";
 
 export default function LogOut() {
-
-
-  const logout = async () => {
+  const logout = useStore((state) => state.logout);
+  
+  const logOut = async () => {
     try {
-      await authService.logOut();
+      const res = await authService.logOut();
+      if(res) {
+        console.log("res", res);
+        logout();
+      }
       console.log("logout");
     } catch (error) {
       console.log("error", error);
     }
-  }
+  };
 
   return (
     <div>
-
       <h1>LogOut</h1>
       <button onClick={logout}>LogOut</button>
     </div>
